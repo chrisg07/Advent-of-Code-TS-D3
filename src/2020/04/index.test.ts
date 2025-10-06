34abbe98b5c565036376414ac98f23b0bff9cdea
Christopher Guilliams <chrisg07@vt.edu>
2025-10-05 19:06:04 -0500

    2020 Day 5 Part 1

--- src/2020/04/index.test.ts
+++ src/2020/04/index.test.ts
@@ -1,14 +1,36 @@
 import { describe, it, expect } from 'vitest';
-import { part1, part2, getInput } from './index';
+import { part1, part2, getInput, parsePassports, parsePairs } from './index';
 
-const exampleInput = `123
-456
+const exampleInput = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
 `;
 
 describe('2020/04', () => {
+	it('should parse passports', async () => {
+		const result = parsePassports(exampleInput);
+		expect(result.length).toBe(4);
+	});
+
+	it('should parse pairs', async () => {
+		const result = parsePassports(exampleInput);
+		expect(result[0].pairs.length).toBe(8);
+		expect(result[2].pairs.length).toBe(7);
+	});
+
 	it('part1 should return expected value for example case', async () => {
 		const result = part1(exampleInput);
-		expect(result).toBe(0);
+		expect(result).toBe(2);
 	});
 
 	it('part1 should return a number', async () => {
@@ -17,14 +39,14 @@ describe('2020/04', () => {
 		expect(result).toBe(0);
 	});
 
-	it('part2 should return expected value for example case', async () => {
-		const result = part2(exampleInput);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return expected value for example case', async () => {
+	// 	const result = part2(exampleInput);
+	// 	expect(result).toBe(0);
+	// });
 	
-	it('part2 should return a number', async () => {
-		const input = await getInput();
-		const result = part2(input);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return a number', async () => {
+	// 	const input = await getInput();
+	// 	const result = part2(input);
+	// 	expect(result).toBe(0);
+	// });
 });
--- src/2020/04/index.ts
+++ src/2020/04/index.ts
@@ -1,4 +1,3 @@
-
 export async function getInput() {
 	// @ts-ignore
 	if (typeof window !== 'undefined' && typeof fetch === 'function') {
@@ -14,9 +13,42 @@ export async function getInput() {
 	}
 }
 
+export  function parsePassports(input: string): {pairs: string[]}[] {
+	const chunks = input.split(/\r?\n\r?\n/)
+	return chunks.map(chunk => { 
+		const lines = chunk.split('\n')
+		const pairs = []
+		for (const line of lines) {
+			const pairsFromLine = line.split(" ")
+			pairs.push(...pairsFromLine)
+		}
+		return {pairs: pairs}
+	})
+}
+
 export function part1(input: string): unknown {
-	// TODO: Implement Part 1 solution
-	return input.length;
+	// const passports = parsePassports(input)
+	
+	const chunks = input.split(/\r?\n\r?\n/);
+	console.log(chunks.length);
+	
+	let validPassports = 0
+	for (const chunk of chunks) {
+		const validBirthYear = chunk.includes('byr')
+		const validIssueYear = chunk.includes('iyr')
+		const validExpirationYear = chunk.includes('eyr')
+		const validHeight = chunk.includes('hgt')
+		const validHairColor = chunk.includes('hcl')
+		const validEyeColor = chunk.includes('ecl')
+		const validPassportID = chunk.includes('pid')
+		const validCountryID = chunk.includes('cid')
+
+		const validRequiredFields = validBirthYear && validExpirationYear && validEyeColor && validHairColor && validHeight && validIssueYear && validPassportID
+		if (validRequiredFields) {
+			validPassports++
+		}
+	}
+	return validPassports;
 }
 
 export function part2(input: string): unknown {
