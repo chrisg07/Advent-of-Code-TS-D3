9e352d5b35c20f8479fa5fb5a57723ecf1790513
Christopher Guilliams <chrisg07@vt.edu>
2025-10-05 22:09:57 -0500

    2020 Day 4 Part 2

--- src/2020/04/index.test.ts
+++ src/2020/04/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { part1, part2, getInput, parsePassports, parsePairs } from './index';
+import { part1, part2, getInput, parsePassports, validateBirthYear, validateIssueYear, validateExpirationYear, validateHairColor, validateHeight, validateEyeColor, validatePassportID } from './index';
 
 const exampleInput = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -14,6 +14,20 @@ hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in
+`;
+
+const examplePart2Input = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
 `;
 
 describe('2020/04', () => {
@@ -36,17 +50,52 @@ describe('2020/04', () => {
 	it('part1 should return a number', async () => {
 		const input = await getInput();
 		const result = part1(input);
-		expect(result).toBe(0);
+		expect(result).toBe(242);
 	});
 
-	// it('part2 should return expected value for example case', async () => {
-	// 	const result = part2(exampleInput);
-	// 	expect(result).toBe(0);
-	// });
+	it('should correctly validate a valid birth year', async () => {
+		const result = validateBirthYear({pairs: ["byr:1980"]});
+		expect(result).toBeTruthy();
+	});
+
+	it('should correctly validate a valid issue year', async () => {
+		const result = validateIssueYear({pairs: ["iyr:2012"]});
+		expect(result).toBeTruthy();
+	});
+
+	it('should correctly validate a valid expiration year', async () => {
+		const result = validateExpirationYear({pairs: ["eyr:2030"]});
+		expect(result).toBeTruthy();
+	});
+
+	it('should correctly validate a valid height in centimeters', async () => {
+		const result = validateHeight({pairs: ["hgt:165cm"]});
+		expect(result).toBeTruthy();
+	});
 	
-	// it('part2 should return a number', async () => {
-	// 	const input = await getInput();
-	// 	const result = part2(input);
-	// 	expect(result).toBe(0);
-	// });
+	it('should correctly validate a valid hair color', async () => {
+		const result = validateHairColor({pairs: ["hcl:#623a2f"]});
+		expect(result).toBeTruthy();
+	});
+
+	it('should correctly validate a valid eye color', async () => {
+		const result = validateEyeColor({pairs: ["ecl:grn"]});
+		expect(result).toBeTruthy();
+	});
+
+	it('should correctly validate a valid passport id', async () => {
+		const result = validatePassportID({pairs: ["pid:087499704"]});
+		expect(result).toBeTruthy();
+	});
+
+	it('part2 should return expected value for example case', async () => {
+		const result = part2(examplePart2Input);
+		expect(result).toBe(4);
+	});
+	
+	it('part2 should return a number', async () => {
+		const input = await getInput();
+		const result = part2(input);
+		expect(result).toBe(186);
+	});
 });
--- src/2020/04/index.ts
+++ src/2020/04/index.ts
@@ -16,22 +16,22 @@ export async function getInput() {
 export  function parsePassports(input: string): {pairs: string[]}[] {
 	const chunks = input.split(/\r?\n\r?\n/)
 	return chunks.map(chunk => { 
-		const lines = chunk.split('\n')
-		const pairs = []
-		for (const line of lines) {
-			const pairsFromLine = line.split(" ")
-			pairs.push(...pairsFromLine)
-		}
-		return {pairs: pairs}
+		return parsePassport(chunk);
 	})
 }
 
-export function part1(input: string): unknown {
-	// const passports = parsePassports(input)
-	
+function parsePassport(chunk: string) {
+	const lines = chunk.split('\n');
+	const pairs = [];
+	for (const line of lines) {
+		const pairsFromLine = line.split(" ").map(line => line.trim());
+		pairs.push(...pairsFromLine);
+	}
+	return { pairs: pairs };
+}
+
+export function part1(input: string): number {
 	const chunks = input.split(/\r?\n\r?\n/);
-	console.log(chunks.length);
-	
 	let validPassports = 0
 	for (const chunk of chunks) {
 		const validBirthYear = chunk.includes('byr')
@@ -51,9 +51,128 @@ export function part1(input: string): unknown {
 	return validPassports;
 }
 
+export function validateBirthYear(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('byr')) {
+			const year = pair.split(":").map(Number)[1]
+			return year >= 1920 && year <= 2002
+		}
+	}
+	return false
+}
+
+export function validateIssueYear(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('iyr')) {
+			const year = pair.split(":").map(Number)[1]
+			return year >= 2010 && year <= 2020
+		}
+	}
+	return false
+}
+
+export function validateExpirationYear(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('eyr')) {
+			const year = pair.split(":").map(Number)[1]
+			return year >= 2020 && year <= 2030
+		}
+	}
+	return false
+}
+
+export function validateHeight(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('hgt') && pair.includes('cm')) {
+			const height = pair.split(":").map(part => part.substring(0, part.length - 2)).map(Number)[1]
+			return height >= 150 && height <= 193
+		} else if (pair.includes('hgt') && pair.includes('in')) {
+			const height = pair.split(":").map(part => part.substring(0, part.length - 2)).map(Number)[1]
+			return height >= 59 && height <= 76
+		}
+	}
+	return false
+}
+
+export function validateHairColor(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('hcl')) {
+			const color = pair.split(":")[1]
+			const startsWithOctothorpe = color.charAt(0) == "#"
+			const validLength = color.length == 7
+			const regexx = /[a-f0-9]/;
+			const validChars = regexx.test(color.substring(1))
+
+			return startsWithOctothorpe && validLength && validChars
+		}
+	}
+	return false
+}
+
+export function validateEyeColor(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('ecl')) {
+			const color = pair.split(":")[1]
+			return color == "amb" || color == "blu" || color == "grn" || color == "brn" || color == "gry" || color == "hzl" || color == "oth"
+		}
+	}
+	return false
+}
+
+export function validatePassportID(passport: {pairs: string[]}): boolean {
+	for (const pair of passport.pairs) {
+		if (pair.includes('pid')) {
+			const id = pair.split(":")[1]
+			const validLength = id.length == 9
+			const regexx = /[0-9]/;
+			const validChars = regexx.test(id)	
+			return validLength && validChars	
+	}
+	}
+	return false
+}
+
 export function part2(input: string): unknown {
-	// TODO: Implement Part 2 solution
-	return input.length;
+	const chunks = input.split(/\r?\n\r?\n/);
+	let validPassports = []
+	for (const chunk of chunks) {
+		const validBirthYear = chunk.includes('byr')
+		const validIssueYear = chunk.includes('iyr')
+		const validExpirationYear = chunk.includes('eyr')
+		const validHeight = chunk.includes('hgt')
+		const validHairColor = chunk.includes('hcl')
+		const validEyeColor = chunk.includes('ecl')
+		const validPassportID = chunk.includes('pid')
+
+		const validRequiredFields = validBirthYear && validExpirationYear && validEyeColor && validHairColor && validHeight && validIssueYear && validPassportID
+		if (validRequiredFields) {
+			validPassports.push(chunk)
+		}
+	}
+	let passportPairs = validPassports.map(chunk => parsePassport(chunk))
+	passportPairs = passportPairs.filter(pairs => {
+		const validBirthYear = validateBirthYear(pairs)
+		const validIssueYear = validateIssueYear(pairs)
+		const validExpirationYear = validateExpirationYear(pairs)
+		const validHeight = validateHeight(pairs)
+		const validHairColor = validateHairColor(pairs)
+		const validEyeColor = validateEyeColor(pairs)
+		const validPassportID = validatePassportID(pairs)
+
+		const validations = [
+			{ name: 'Birth Year', valid: validBirthYear },
+			{ name: 'Issue Year', valid: validIssueYear },
+			{ name: 'Expiration Year', valid: validExpirationYear },
+			{ name: 'Height', valid: validHeight },
+			{ name: 'Hair Color', valid: validHairColor },
+			{ name: 'Eye Color', valid: validEyeColor },
+			{ name: 'Passport ID', valid: validPassportID },
+		];
+		const failed = validations.filter(v => !v.valid);
+		const isValidPassport = failed.length === 0;
+		return isValidPassport;
+	})
+	return passportPairs.length;
 }
 
 // Only run this block in Node.js
