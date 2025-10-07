ab691afc1e131ef92cef8e4f65d6cf917e2ab539
Christopher Guilliams <chrisg07@vt.edu>
2025-10-06 21:12:58 -0500

    2020 Day 7 Part 1

--- src/2020/07/index.test.ts
+++ src/2020/07/index.test.ts
@@ -1,30 +1,69 @@
 import { describe, it, expect } from 'vitest';
-import { part1, part2, getInput } from './index';
+import { part1, part2, getInput, parseNode } from './index';
 
-const exampleInput = `123
-456
+const exampleInput = `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
 `;
 
 describe('2020/07', () => {
+	it('should be able to parse node from line of input', async () => {
+		const result = parseNode("light red bags contain 1 bright white bag, 2 muted yellow bags.");
+		const node = {
+			value: "light red",
+			children: [
+				{ value: "bright white", weight: 1},
+				{ value: "muted yellow", weight: 2}
+			]
+		}
+		expect(result).toEqual(node);
+	});
+
+	it('should be able to parse node from line of input', async () => {
+		const result = parseNode("bright white bags contain 1 shiny gold bag.");
+		const node = {
+			value: "bright white",
+			children: [
+				{ value: "shiny gold", weight: 1}
+			]
+		}
+		expect(result).toEqual(node);
+	});
+
+	it('should be able to parse node from line of input', async () => {
+		const result = parseNode("faded blue bags contain no other bags.");
+		const node = {
+			value: "faded blue",
+			children: []
+		}
+		expect(result).toEqual(node);
+	});
+	
 	it('part1 should return expected value for example case', async () => {
 		const result = part1(exampleInput);
-		expect(result).toBe(0);
+		expect(result).toBe(4);
 	});
 
 	it('part1 should return a number', async () => {
 		const input = await getInput();
 		const result = part1(input);
-		expect(result).toBe(0);
+		expect(result).toBe(274);
 	});
 
-	it('part2 should return expected value for example case', async () => {
-		const result = part2(exampleInput);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return expected value for example case', async () => {
+	// 	const result = part2(exampleInput);
+	// 	expect(result).toBe(0);
+	// });
 	
-	it('part2 should return a number', async () => {
-		const input = await getInput();
-		const result = part2(input);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return a number', async () => {
+	// 	const input = await getInput();
+	// 	const result = part2(input);
+	// 	expect(result).toBe(0);
+	// });
 });
--- src/2020/07/index.ts
+++ src/2020/07/index.ts
@@ -14,9 +14,80 @@ export async function getInput() {
 	}
 }
 
+type BagNode = {
+	value: string,
+	children: {value: string, weight: number}[]
+}
+
+export  function parseNode(line: string): BagNode {
+	const lineParts = line.split("contain")
+	const color = lineParts[0].substring(0, lineParts[0].length - 6)
+	
+	if (line.includes("no other bags")) {
+		return {
+			value: color,
+			children: []
+		}
+	}
+
+	const children: {value: string, weight: number}[] = []
+	const childLineParts = lineParts[1].split(",").map(part => part.trim())
+	for (const childPart of childLineParts) {
+		const childNodeBag = {
+			value: childPart.split("bag")[0].substring(2).trim(),
+			weight: [childPart.substring(0, 1)].map(Number)[0]
+		}
+		children.push(childNodeBag)
+	}
+
+	return {
+		value: color,
+		children: children
+	}
+}
+
+interface BagMap {
+  [key: string]: BagNode;
+}
+
+function parseNodes(input: string): BagMap {
+	const bagMap: BagMap = {}
+
+	const lines = input.split("\n")
+	for (const line of lines) {
+		const node = parseNode(line)
+		bagMap[node.value] = node
+	}
+
+	return bagMap
+} 
+
+let nodes: BagMap = {}
+
+function dfs(node: BagNode): boolean {
+	if (node.children.length == 0) {
+		return false
+	}
+
+	if (node.value == "shiny gold") return true
+	
+	for (const child of node.children) {
+		if (dfs(nodes[child.value])) return true
+	}
+	return false
+}
+
 export function part1(input: string): unknown {
-	// TODO: Implement Part 1 solution
-	return input.length;
+	nodes = parseNodes(input.trim())
+
+	let subTreesThatIncludeShinyGoldBag = 0
+	for (const [key, value] of Object.entries(nodes)) {
+		if (dfs(nodes[value.value])) {
+			subTreesThatIncludeShinyGoldBag++
+		}
+	}
+
+	return subTreesThatIncludeShinyGoldBag - 1
 }
 
 export function part2(input: string): unknown {
