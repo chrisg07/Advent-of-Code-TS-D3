2a854dbe6e0b61b2123f0a85eea8380d80c757ea
Christopher Guilliams <chrisg07@vt.edu>
2025-10-12 18:12:08 -0500

    2020 Day 11 Part 1

--- src/2020/11/index.test.ts
+++ src/2020/11/index.test.ts
@@ -1,30 +1,71 @@
 import { describe, it, expect } from 'vitest';
-import { part1, part2, getInput } from './index';
+import { part1, part2, getInput, calculateRound } from './index';
 
-const exampleInput = `123
-456
+const exampleInput = `L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
 `;
 
+const roundTwo = `#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##
+`
+
+const roundThree = `#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##
+`
 describe('2020/11', () => {
+	it('should be able to calculate state change for first turn', async () => {
+		const result = calculateRound(exampleInput.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundTwo.trim().split("\n").join(""));
+	});
+
+	it('should be able to calculate state change for second turn', async () => {
+		const result = calculateRound(roundTwo.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundThree.trim().split("\n").join(""));
+	});
+
 	it('part1 should return expected value for example case', async () => {
 		const result = part1(exampleInput);
-		expect(result).toBe(0);
+		expect(result).toBe(37);
 	});
 
 	it('part1 should return a number', async () => {
 		const input = await getInput();
 		const result = part1(input);
-		expect(result).toBe(0);
+		expect(result).toBe(2368);
 	});
 
-	it('part2 should return expected value for example case', async () => {
-		const result = part2(exampleInput);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return expected value for example case', async () => {
+	// 	const result = part2(exampleInput);
+	// 	expect(result).toBe(0);
+	// });
 	
-	it('part2 should return a number', async () => {
-		const input = await getInput();
-		const result = part2(input);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return a number', async () => {
+	// 	const input = await getInput();
+	// 	const result = part2(input);
+	// 	expect(result).toBe(0);
+	// });
 });
--- src/2020/11/index.ts
+++ src/2020/11/index.ts
@@ -14,9 +14,61 @@ export async function getInput() {
 	}
 }
 
+export function calculateRound(model: string, width: number): string {
+	let updatedModel = ""
+
+	for (let i = 0; i < model.length; i++) {
+		const seat = model.charAt(i);
+
+		const isOnLeftEdge = i % width === 0
+		const isOnRightEdge = i % width === width - 1
+		const neighborIndices = [
+			!isOnLeftEdge ? i - 1 : -1, // western (unless on left edge of grid)
+			!isOnRightEdge ? i + 1 : -1, // eastern (unless on right edge of grid)
+			i - width, // northern
+			!isOnLeftEdge ? i - width - 1 : -1, // northwestern (unless on left edge of grid)
+			!isOnRightEdge ? i - width + 1 : -1, // northeastern (unless on right edge of grid)
+			i + width, // southern
+			!isOnLeftEdge ? i + width - 1 : -1, // southerwestern (unless on left edge of grid)
+			!isOnRightEdge ? i + width + 1 : -1, // southeastern (unless on right edge of grid)
+		]
+		const validNeighborIndices = neighborIndices.filter(index => {
+			return index >= 0 && index <= model.length - 1
+		})
+
+		let occupiedNeighbors = 0
+		let emptyNeighbors = 0
+		validNeighborIndices.forEach(index => {
+			if (model.charAt(index) === "#") {
+				occupiedNeighbors++
+			}
+		})
+		if (seat === "L" && occupiedNeighbors === 0) {
+			updatedModel += "#"
+		} else if (seat === "#" && occupiedNeighbors >= 4) {
+			updatedModel += "L"
+		} else {
+			updatedModel += seat
+		}
+	}
+
+	return updatedModel
+}
+
 export function part1(input: string): unknown {
-	// TODO: Implement Part 1 solution
-	return input.length;
+	const lines = input.trim().split("\n")
+	const width = lines[0].length
+	let startingModel = lines.join("")
+	
+	let nextRoundModel = calculateRound(startingModel, width)
+	
+	while (nextRoundModel != startingModel) {
+		startingModel = nextRoundModel
+		nextRoundModel = calculateRound(nextRoundModel, width)
+	}
+
+	const occupiedSeats = nextRoundModel.split("").filter(seat => seat === "#").length 
+	return occupiedSeats
 }
 
 export function part2(input: string): unknown {
