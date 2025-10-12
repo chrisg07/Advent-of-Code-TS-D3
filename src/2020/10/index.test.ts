3cb0a28aa508bb1c5c6b5ad095d01062ab13a97b
Christopher Guilliams <chrisg07@vt.edu>
2025-10-11 20:25:50 -0500

    setup

--- src/2020/10/index.test.ts
+++ src/2020/10/index.test.ts
@@ -1,30 +1,39 @@
 import { describe, it, expect } from 'vitest';
 import { part1, part2, getInput } from './index';
 
-const exampleInput = `123
-456
+const exampleInput = `16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4
 `;
 
 describe('2020/10', () => {
 	it('part1 should return expected value for example case', async () => {
 		const result = part1(exampleInput);
-		expect(result).toBe(0);
+		expect(result).toBe(220);
 	});
 
-	it('part1 should return a number', async () => {
-		const input = await getInput();
-		const result = part1(input);
-		expect(result).toBe(0);
-	});
+	// it('part1 should return a number', async () => {
+	// 	const input = await getInput();
+	// 	const result = part1(input);
+	// 	expect(result).toBe(0);
+	// });
 
-	it('part2 should return expected value for example case', async () => {
-		const result = part2(exampleInput);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return expected value for example case', async () => {
+	// 	const result = part2(exampleInput);
+	// 	expect(result).toBe(0);
+	// });
 	
-	it('part2 should return a number', async () => {
-		const input = await getInput();
-		const result = part2(input);
-		expect(result).toBe(0);
-	});
+	// it('part2 should return a number', async () => {
+	// 	const input = await getInput();
+	// 	const result = part2(input);
+	// 	expect(result).toBe(0);
+	// });
 });
