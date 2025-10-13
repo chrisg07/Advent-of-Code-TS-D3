d332e11154591183ebdf727e1230ce24541331c3
Christopher Guilliams <chrisg07@vt.edu>
2025-10-12 20:46:27 -0500

    2020 Day 11 Part 2

--- src/2020/11/index.test.ts
+++ src/2020/11/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { part1, part2, getInput, calculateRound } from './index';
+import { part1, part2, getInput, calculateRound, calculateRoundPart2 } from './index';
 
 const exampleInput = `L.LL.LL.LL
 LLLLLLL.LL
@@ -36,6 +36,67 @@ L.L.L..L..
 #.LLLLLL.L
 #.#LLLL.##
 `
+
+const roundThreePartTwo = `#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#
+`
+
+const roundFourPartTwo = `#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#
+`
+
+const roundFivePartTwo = `#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#
+`
+
+const roundSixPartTwo = `#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+`
+
+const roundSevenPartTwo = `#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+`
+
 describe('2020/11', () => {
 	it('should be able to calculate state change for first turn', async () => {
 		const result = calculateRound(exampleInput.trim().split("\n").join(""), 10);
@@ -58,14 +119,44 @@ describe('2020/11', () => {
 		expect(result).toBe(2368);
 	});
 
-	// it('part2 should return expected value for example case', async () => {
-	// 	const result = part2(exampleInput);
-	// 	expect(result).toBe(0);
-	// });
+	it('part 2 should be able to calculate state change for first turn', async () => {
+		const result = calculateRoundPart2(exampleInput.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundTwo.trim().split("\n").join(""));
+	});
+
+	it('part 2 should be able to calculate state change for second turn', async () => {
+		const result = calculateRoundPart2(roundTwo.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundThreePartTwo.trim().split("\n").join(""));
+	});
+
+	it('part 2 should be able to calculate state change for third turn', async () => {
+		const result = calculateRoundPart2(roundThreePartTwo.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundFourPartTwo.trim().split("\n").join(""));
+	});
+
+	it('part 2 should be able to calculate state change for fourth turn', async () => {
+		const result = calculateRoundPart2(roundFourPartTwo.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundFivePartTwo.trim().split("\n").join(""));
+	});
+
+	it('part 2 should be able to calculate state change for fifth turn', async () => {
+		const result = calculateRoundPart2(roundFivePartTwo.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundSixPartTwo.trim().split("\n").join(""));
+	});
+
+	it('part 2 should be able to calculate state change for sixth turn', async () => {
+		const result = calculateRoundPart2(roundSixPartTwo.trim().split("\n").join(""), 10);
+		expect(result).toBe(roundSevenPartTwo.trim().split("\n").join(""));
+	});
+
+	it('part2 should return expected value for example case', async () => {
+		const result = part2(exampleInput);
+		expect(result).toBe(26);
+	});
 	
-	// it('part2 should return a number', async () => {
-	// 	const input = await getInput();
-	// 	const result = part2(input);
-	// 	expect(result).toBe(0);
-	// });
+	it('part2 should return a number', async () => {
+		const input = await getInput();
+		const result = part2(input);
+		expect(result).toBe(2124);
+	});
 });
--- src/2020/11/index.ts
+++ src/2020/11/index.ts
@@ -71,9 +71,120 @@ export function part1(input: string): unknown {
 	return occupiedSeats
 }
 
+export function calculateRoundPart2(model: string, width: number): string {
+	let updatedModel = ""
+
+	for (let i = 0; i < model.length; i++) {
+		const seat = model.charAt(i);
+
+		const isOnLeftEdge = i % width === 0
+		const isOnRightEdge = i % width === width - 1
+
+		let westernVisibleSeat = !isOnLeftEdge ? i - 1 : -1;
+		while (!isSeatAtIndex(model, westernVisibleSeat) && westernVisibleSeat != -1) {
+			const onLeftEdge = westernVisibleSeat % width === 0
+			westernVisibleSeat = !onLeftEdge ? westernVisibleSeat - 1 : -1;
+		}
+
+		let easternVisibleSeat = !isOnRightEdge ? i + 1 : -1;
+		while (!isSeatAtIndex(model, easternVisibleSeat) && easternVisibleSeat != -1) {
+			const onRightEdge = easternVisibleSeat % width === width - 1
+			easternVisibleSeat = !onRightEdge ? easternVisibleSeat + 1 : -1;
+		}
+
+		let northernVisibleSeat = i - width;
+		while (!isSeatAtIndex(model, northernVisibleSeat) && northernVisibleSeat != -1) {
+			const onNorthEdge = northernVisibleSeat < 0
+			northernVisibleSeat = !onNorthEdge ? northernVisibleSeat - width : -1;
+		}
+
+		// I think this is okay
+		let northwesternVisibleSeat = !isOnLeftEdge ? i - width - 1 : -1;
+		while (!isSeatAtIndex(model, northwesternVisibleSeat) && northwesternVisibleSeat != -1) {
+			const onNorthEdge = northwesternVisibleSeat < 0
+			const onLeftEdge = northwesternVisibleSeat % width === 0
+			northwesternVisibleSeat = !onNorthEdge && !onLeftEdge ? northwesternVisibleSeat - width - 1 : -1;
+		}
+
+		let northeasternVisibleSeat = !isOnRightEdge ? i - width + 1 : -1;
+		while (!isSeatAtIndex(model, northeasternVisibleSeat) && northeasternVisibleSeat != -1) {
+			const onNorthEdge = northeasternVisibleSeat < 0
+			const onRightEdge = northeasternVisibleSeat % width === width - 1
+			northeasternVisibleSeat = !onNorthEdge && !onRightEdge ? northeasternVisibleSeat - width + 1 : -1;
+		}
+
+		let southernVisibleSeat = i + width;
+		while (!isSeatAtIndex(model, southernVisibleSeat) && southernVisibleSeat != -1) {
+			const onSouthEdge = southernVisibleSeat > model.length
+			southernVisibleSeat = !onSouthEdge ? southernVisibleSeat + width : -1;
+		}
+
+		let southwesternVisibleSeat = !isOnLeftEdge ? i + width - 1 : -1;
+		while (!isSeatAtIndex(model, southwesternVisibleSeat) && southwesternVisibleSeat != -1) {
+			const onSouthEdge = southwesternVisibleSeat > model.length
+			const onLeftEdge = southwesternVisibleSeat % width === 0
+			southwesternVisibleSeat = !onSouthEdge && !onLeftEdge ? southwesternVisibleSeat + width - 1 : -1;
+		}
+		
+		let southeasternVisibleSeat = !isOnRightEdge ? i + width + 1 : -1;
+		while (!isSeatAtIndex(model, southeasternVisibleSeat) && southeasternVisibleSeat != -1) {
+			const onSouthEdge = southeasternVisibleSeat > model.length
+			const onRightEdge = southeasternVisibleSeat % width === width - 1
+			southeasternVisibleSeat = !onSouthEdge && !onRightEdge ? southeasternVisibleSeat + width + 1 : -1;
+		}
+
+		const neighborIndices = [
+			westernVisibleSeat, // western (unless on left edge of grid)
+			easternVisibleSeat, // eastern (unless on right edge of grid)
+			northernVisibleSeat, // northern
+			northwesternVisibleSeat, // northwestern (unless on left edge of grid)
+			northeasternVisibleSeat, // northeastern (unless on right edge of grid)
+			southernVisibleSeat, // southern
+			southwesternVisibleSeat, // southerwestern (unless on left edge of grid)
+			southeasternVisibleSeat, // southeastern (unless on right edge of grid)
+		]
+
+		const visibleNeighborIndices = neighborIndices.filter(index => {
+			return index >= 0 && index <= model.length - 1
+		})
+
+		let occupiedNeighbors = 0
+		let emptyNeighbors = 0
+		visibleNeighborIndices.forEach(index => {
+			if (model.charAt(index) === "#") {
+				occupiedNeighbors++
+			}
+		})
+		if (seat === "L" && occupiedNeighbors === 0) {
+			updatedModel += "#"
+		} else if (seat === "#" && occupiedNeighbors >= 5) {
+			updatedModel += "L"
+		} else {
+			updatedModel += seat
+		}
+	}
+
+	return updatedModel
+}
+
+function isSeatAtIndex(model: string, westernVisibleSeat: number) {
+	return model.charAt(westernVisibleSeat) == "#" || model.charAt(westernVisibleSeat) == "L";
+}
+
 export function part2(input: string): unknown {
-	// TODO: Implement Part 2 solution
-	return input.length;
+	const lines = input.trim().split("\n")
+	const width = lines[0].length
+	let startingModel = lines.join("")
+	
+	let nextRoundModel = calculateRoundPart2(startingModel, width)
+	
+	while (nextRoundModel != startingModel) {
+		startingModel = nextRoundModel
+		nextRoundModel = calculateRoundPart2(nextRoundModel, width)
+	}
+
+	const occupiedSeats = nextRoundModel.split("").filter(seat => seat === "#").length 
+	return occupiedSeats
 }
 
 // Only run this block in Node.js
