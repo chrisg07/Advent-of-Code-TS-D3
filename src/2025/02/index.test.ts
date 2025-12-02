333a4d8d1d1c36b9d7fceb9dbbfdcca70a81aac9
Christopher Guilliams <chrisg07@vt.edu>
2025-12-01 23:32:20 -0600

    2025 Day 2 Part 1

--- src/2025/02/index.test.ts
+++ src/2025/02/index.test.ts
@@ -1,31 +1,32 @@
-import { describe, it, expect } from 'vitest';
-import { part1, part2 } from './index';
-import { getInput } from 'src/utils';
+import { describe, it, expect } from "vitest";
+import { part1, part2 } from "./index";
+import { getInput } from "src/utils";
 
-const exampleInput = `123
-456
+const exampleInput = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
 `;
 
-describe('2025/02', () => {
-	it('part1 should return expected value for example case', async () => {
-		const result = part1(exampleInput);
-		expect(result).toBe(0);
-	});
+describe("2025/02", () => {
+  it("part1 should return expected value for example case", async () => {
+    const result = part1(exampleInput);
+    expect(result).toBe(1227775554);
+  });
 
-	it('part1 should return a number', async () => {
-		const input = await getInput(import.meta.url);
-		const result = part1(input);
-		expect(result).toBe(0);
-	});
+  it("part1 should return a number", async () => {
+    const input = await getInput(import.meta.url);
+    const result = part1(input);
+    expect(result).toBe(12850231731);
+  });
 
-	it('part2 should return expected value for example case', async () => {
-		const result = part2(exampleInput);
-		expect(result).toBe(0);
-	});
-	
-	it('part2 should return a number', async () => {
-		const input = await getInput(import.meta.url);
-		const result = part2(input);
-		expect(result).toBe(0);
-	});
+  // it('part2 should return expected value for example case', async () => {
+  // 	const result = part2(exampleInput);
+  // 	expect(result).toBe(0);
+  // });
+
+  // it('part2 should return a number', async () => {
+  // 	const input = await getInput(import.meta.url);
+  // 	const result = part2(input);
+  // 	expect(result).toBe(0);
+  // });
 });
--- src/2025/02/index.ts
+++ src/2025/02/index.ts
@@ -1,35 +1,58 @@
-import { getInput } from '../../utils';
+import { getInput, parseNumbersFromString } from "../../utils";
 
 export function part1(input: string): unknown {
-	// TODO: Implement Part 1 solution
-    let answer = 0
-	const lines = input.trim().split('\n')
+  const ids = parseNumbersFromString(input).map((id) => id.toString());
+  const invalidIds = [];
 
-	for (let i = 0; i < lines.length; i++) {
-		const line = lines[i];
-		
-	}
-	return answer;
+  for (let i = 0; i < ids.length; i += 2) {
+    const left = Number(ids[i]);
+    const right = Number(ids[i + 1]);
+
+    for (let id = left; id <= right; id++) {
+      const invalidId = isInvalid(id.toString());
+      if (invalidId) {
+        invalidIds.push(id);
+      }
+    }
+  }
+
+  return invalidIds.map(Number).reduce((acc, val) => acc + val);
+}
+
+function isInvalid(id: string): boolean {
+  if (id.length % 2 != 0) return false;
+
+  const midpoint = Math.floor(id.length / 2);
+  const left = id.substring(0, midpoint);
+  const right = id.substring(midpoint);
+
+  return left == right;
 }
 
 export function part2(input: string): unknown {
-	// TODO: Implement Part 2 solution
-    let answer = 0
-	const lines = input.trim().split('\n')
+  // TODO: Implement Part 2 solution
+  let answer = 0;
+  const ids = parseNumbersFromString(input);
 
-	for (let i = 0; i < lines.length; i++) {
-		const line = lines[i];
-		
-	}
-	return answer;
+  for (let i = 0; i < ids.length; i += 2) {
+    const left = ids[i];
+    const right = ids[i + 1];
+  }
+  return answer;
 }
 
 // Only run this block in Node.js
-if (typeof process !== 'undefined' && process.release && process.release.name === 'node' && typeof require !== 'undefined' && require.main === module) {
+if (
+  typeof process !== "undefined" &&
+  process.release &&
+  process.release.name === "node" &&
+  typeof require !== "undefined" &&
+  require.main === module
+) {
   (async () => {
-	const { resolve } = await import('node:path');
-	const input = await getInput(import.meta.url);
-	console.log('Part 1:', part1(input));
-	console.log('Part 2:', part2(input));
+    const { resolve } = await import("node:path");
+    const input = await getInput(import.meta.url);
+    console.log("Part 1:", part1(input));
+    console.log("Part 2:", part2(input));
   })();
 }
