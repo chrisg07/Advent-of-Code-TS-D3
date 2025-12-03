76a41900251a2926f4d8ff3204cb38c384d1f40a
Christopher Guilliams <chrisg07@vt.edu>
2025-12-03 16:37:32 -0600

    2025 Day 3 Part 2, greedy solution unable to solve full input

--- src/2025/03/index.test.ts
+++ src/2025/03/index.test.ts
@@ -20,14 +20,14 @@ describe("2025/03", () => {
     expect(result).toBe(17193);
   });
 
-  // it('part2 should return expected value for example case', async () => {
-  // 	const result = part2(exampleInput);
-  // 	expect(result).toBe(0);
-  // });
+  it("part2 should return expected value for example case", async () => {
+    const result = part2(exampleInput);
+    expect(result).toBe(3121910778619);
+  });
 
-  // it('part2 should return a number', async () => {
-  // 	const input = await getInput(import.meta.url);
-  // 	const result = part2(input);
-  // 	expect(result).toBe(0);
-  // });
+  it("part2 should return a number", async () => {
+    const input = await getInput(import.meta.url);
+    const result = part2(input);
+    expect(result).toBe(0);
+  });
 });
--- src/2025/03/index.ts
+++ src/2025/03/index.ts
@@ -27,13 +27,72 @@ export function part1(input: string): unknown {
 }
 
 export function part2(input: string): unknown {
-  // TODO: Implement Part 2 solution
   let answer = 0;
   const lines = input.trim().split("\n");
+  const banks = lines.map((line) => line.split("").map(Number));
+  const bankTotals: number[] = [];
 
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
+  for (let index = 0; index < banks.length; index++) {
+    const bank = banks[index];
+    bankTotals[index] = 0;
+    for (let i = 0; i < bank.length - 1; i++) {
+      const bat1 = bank[i];
+      for (let j = i + 1; j < bank.length; j++) {
+        const bat2 = bank[j];
+        for (let k = j + 1; k < bank.length; k++) {
+          const bat3 = bank[k];
+          for (let l = k + 1; l < bank.length; l++) {
+            const bat4 = bank[l];
+            for (let m = l + 1; m < bank.length; m++) {
+              const bat5 = bank[m];
+              for (let n = m + 1; n < bank.length; n++) {
+                const bat6 = bank[n];
+                for (let o = n + 1; o < bank.length; o++) {
+                  const bat7 = bank[o];
+                  for (let p = o + 1; p < bank.length; p++) {
+                    const bat8 = bank[p];
+                    for (let q = p + 1; q < bank.length; q++) {
+                      const bat9 = bank[q];
+                      for (let r = q + 1; r < bank.length; r++) {
+                        const bat10 = bank[r];
+                        for (let s = r + 1; s < bank.length; s++) {
+                          const bat11 = bank[s];
+                          for (let t = s + 1; t < bank.length; t++) {
+                            const bat12 = bank[t];
+                            const batteries = [
+                              bat1,
+                              bat2,
+                              bat3,
+                              bat4,
+                              bat5,
+                              bat6,
+                              bat7,
+                              bat8,
+                              bat9,
+                              bat10,
+                              bat11,
+                              bat12,
+                            ];
+                            const total = Number(batteries.join(""));
+                            if (bankTotals[index] < total) {
+                              bankTotals[index] = total;
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
+
+  answer = bankTotals.reduce((acc, val) => acc + val);
+
   return answer;
 }
 
