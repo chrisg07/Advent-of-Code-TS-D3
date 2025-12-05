0543d493f04677e93ae5bbe811a28ae875029005
Chris Guilliams <chrisg07@vt.edu>
2025-12-05 17:44:29 -0600

    2025 Day 5 Part 2

--- src/2025/05/index.ts
+++ src/2025/05/index.ts
@@ -20,7 +20,6 @@ export function part1(input: string): unknown {
 			const right = rightRanges[j]
 
 			if (id >= left && id <= right) {
-				console.log(id, " is fresh");
 				answer++
 				break
 			}
@@ -29,15 +28,73 @@ export function part1(input: string): unknown {
 	return answer;
 }
 
-export function part2(input: string): unknown {
-	// TODO: Implement Part 2 solution
-    let answer = 0
-	const lines = input.trim().split('\n')
+function updateRanges(leftRanges: number[], rightRanges: number[]) {
+	for (let i = 0; i < leftRanges.length; i++) {
+		const left = leftRanges[i];
+		const right = rightRanges[i];
 
-	for (let i = 0; i < lines.length; i++) {
-		const line = lines[i];
-		
+		for (let j = 0; j < leftRanges.length; j++) {
+			if (i != j && (leftRanges[i] != 0 && rightRanges[i] != 0 && (leftRanges[j] != 0 && rightRanges[j] != 0))) {
+				const leftNestedRange = leftRanges[j];
+				const rightNestedRange = rightRanges[j];
+				// 443_507_857_187_033
+				// 100_000_000_000_000
+				if (Math.abs(right - leftNestedRange) < 500_000_000 || Math.abs(left - rightNestedRange) < 500_000_000) {
+					// console.log("Comparing ", left, " ", right, " to ", leftNestedRange, " ", rightNestedRange);
+				}
+				if (left >= leftNestedRange && right <= rightNestedRange) {
+					// console.log("1 - Reset range: ", left, right, " to: 0-0");
+					leftRanges[i] = 0;
+					rightRanges[i] = 0;
+				} else if (left <= rightNestedRange && rightNestedRange >= left && leftNestedRange <= right && left >= leftNestedRange) {
+					// console.log("2 - Reset range: ", left, right, " to: ", leftNestedRange, right);
+					leftRanges[i] = leftNestedRange;
+				} else if (right >= leftNestedRange && leftNestedRange <= right && rightNestedRange >= left && right <= rightNestedRange) {
+					// console.log("3 - Reset range: ", left, right, " to: ", left, rightNestedRange);
+					rightRanges[i] = rightNestedRange;
+				} else if (left == rightNestedRange || right == leftNestedRange && rightNestedRange == leftNestedRange) {
+					// console.log("4 - Reset range: ", left, right, " to: 0-0");
+					leftRanges[i] = 0;
+					rightRanges[i] = 0;
+				} 
+			}
+		}
 	}
+}
+
+export function part2(input: string): unknown {
+    let answer = 0
+	const splitInput = input.trim().split('\n\n')
+	
+	const rangeStrings = splitInput[0].split('\n')
+	const rangesList = rangeStrings.map(range => range.split('-'))
+	const ranges = rangesList.map(ranges => ranges.map(Number))
+	const leftRanges = ranges.map(range => range[0])
+	const rightRanges = ranges.map(range => range[1])
+
+	let previousSum = calculateSumOfRanges(leftRanges, rightRanges)
+	updateRanges(leftRanges, rightRanges)
+	updateRanges(leftRanges, rightRanges)
+
+	while (calculateSumOfRanges(leftRanges, rightRanges) != previousSum) {
+		previousSum = calculateSumOfRanges(leftRanges, rightRanges)
+		updateRanges(leftRanges, rightRanges)
+		updateRanges(leftRanges, rightRanges)
+	}
+
+	let pairs: [left: number, right: number][] = []
+	for (let i = 0; i < leftRanges.length; i++) {
+		if (leftRanges[i] != 0 && rightRanges[i] != 0) {
+			pairs.push([leftRanges[i], rightRanges[i]])
+		}
+	}
+	
+	pairs.sort((a, b) => a[0] - b[0])
+	// console.log(leftRanges, rightRanges);
+	// console.log(pairs);
+	
+	answer = calculateSumOfRanges(leftRanges, rightRanges);
+
 	return answer;
 }
 
@@ -50,3 +107,20 @@ if (typeof process !== 'undefined' && process.release && process.release.name ==
 	console.log('Part 2:', part2(input));
   })();
 }
+
+
+function calculateSumOfRanges(leftRanges: number[], rightRanges: number[]) {
+	let sum = 0
+	for (let i = 0; i < leftRanges.length; i++) {
+		const left = leftRanges[i];
+		const right = rightRanges[i];
+
+		if (right - left != 0) {
+			const delta = (right - left) + 1;
+			// console.log("adding to asnwer: ", delta);
+			sum += delta;
+		} else if (right - left == 0 && (right != 0 && left != 0)) sum++
+	}
+	return sum;
+}
+
