8a61c1fd114ad5588271b3cbd4cf0c9d453037b3
Christopher Guilliams <chrisg07@vt.edu>
2025-12-05 23:01:33 -0600

    scaffolded 2025 Day 6

---
+++ src/2025/06/index.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from "vitest";
+import { part1, part2 } from "./index";
+import { getInput } from "src/utils";
+
+const exampleInput = `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+`;
+
+describe("2025/06", () => {
+  it("part1 should return expected value for example case", async () => {
+    const result = part1(exampleInput);
+    expect(result).toBe(0);
+  });
+
+  // it('part1 should return a number', async () => {
+  // 	const input = await getInput(import.meta.url);
+  // 	const result = part1(input);
+  // 	expect(result).toBe(0);
+  // });
+
+  // it('part2 should return expected value for example case', async () => {
+  // 	const result = part2(exampleInput);
+  // 	expect(result).toBe(0);
+  // });
+
+  // it('part2 should return a number', async () => {
+  // 	const input = await getInput(import.meta.url);
+  // 	const result = part2(input);
+  // 	expect(result).toBe(0);
+  // });
+});
