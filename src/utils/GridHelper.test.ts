666da42405b42ddd2e9939006acacc85678f4003
Chris Guilliams <chrisg07@vt.edu>
2025-12-06 11:58:34 -0600

    initial failing tests for expanding range of neighbor selection for GridHelper class

--- src/utils/GridHelper.test.ts
+++ src/utils/GridHelper.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-import { getInput } from 'src/utils/utils';
 import { GridHelper } from './GridHelper';
 
 const exampleInput = `..@@.@@@@.
@@ -14,6 +13,13 @@ const exampleInput = `..@@.@@@@.
 @.@.@@@.@.
 `;
 
+const expandedNeighborRangeInput = `.....
+.....
+..@..
+.....
+.....
+`;
+
 describe('Grid helper', () => {
 	it('should be able to return a string representation of the cells of the grid', async () => {
 		const grid = new GridHelper(exampleInput);
@@ -23,5 +29,17 @@ describe('Grid helper', () => {
     it('should be able to return the number of characters matching a value in the grid', async () => {
 		const grid = new GridHelper(exampleInput);
         expect(grid.getCount("@")).toEqual(71)
+	});
+
+    it('should be able to return the number of neighbors within a configurable range of a cell', async () => {
+		const grid = new GridHelper(expandedNeighborRangeInput, 1);
+        const cell = grid.cells.filter(cell => cell.value === '@')[0]
+        expect(cell.neighbors.length).toBe(8)
+	});
+
+    it('should be able to return the number of neighbors within a configurable range of a cell', async () => {
+		const grid = new GridHelper(expandedNeighborRangeInput, 2);
+        const cell = grid.cells.filter(cell => cell.value === '@')[0]
+        expect(cell.neighbors.length).toBe(24)
 	});
 });
